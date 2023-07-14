3bc655c296081454c7ff7704ae2aebe33ebbcf7b
EroGold <lson4718@gmail.com>
2023-07-14 09:36:58 +0700

    update

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -29,16 +29,21 @@ $(".js-close").click(function(){
 
 // fixed nav
 
-        $(window).scroll(function(){
-            if ($(window).scrollTop() >= 100) {
-                $('nav').addClass('fixed-header');
-                $('nav div').addClass('visible-title');
-            }
-            else {
-                $('nav').removeClass('fixed-header');
-                $('nav div').removeClass('visible-title');
-            }
-        });
+window.onscroll = function(){
+    myFunction()
+}
+
+var nav = document.getElementById('myNav')
+var sticky = nav.offsetTop
+
+function myFunction(){
+    if (window.pageYOffset > sticky) {
+        nav.classList.add("sticky");
+      } else {
+        nav.classList.remove("sticky");
+        }
+    }
+
 
 
 // Slider
@@ -94,3 +99,4 @@ $(document).ready(function() {
             }
           });
         });
+
