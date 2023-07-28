ecfd9e1ddbf014c28e520d74c984d6b0a1e0ba93
EroGold <lson4718@gmail.com>
2023-07-28 11:54:17 +0700

    update

--- asset/js/detailjs.js
+++ asset/js/detailjs.js
@@ -27,3 +27,17 @@ $('.view-more-container a').click(function(){
         })
     }
 })
+
+// glider img   
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.5,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        }
+      });
+    });
