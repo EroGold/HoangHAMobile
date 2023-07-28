52e190da14c8f9ee4197f6728500f5fd29adf6c7
EroGold <lson4718@gmail.com>
2023-07-28 11:15:50 +0700

    update

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -112,18 +112,3 @@ $(document).ready(function() {
         }, 5000);
     });
 
-// Glider product
-
-    window.addEventListener('load', function(){
-        new Glider(document.querySelector('.glider'), {
-            slidesToScroll: 1,
-            slidesToShow: 5,
-            draggable: true,
-            arrows: {
-              prev: '.glider-prev',
-              next: '.glider-next'
-            }
-          });
-        });
-
-// Show more
